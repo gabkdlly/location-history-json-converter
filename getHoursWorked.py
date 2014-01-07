b28dd2434398f0d67a0324a35f7cb00472a270ee
Gabriel Pannwitz <gabriel.pannwitz@googlemail.com>
2014-01-07 17:14:00 +0100

    Inserted web link and legal notice.

--- getHoursWorked.py
+++ getHoursWorked.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python
 
-# Copyright 2012 Gerwin Sturm, FoldedSoft e.U. / www.foldedsoft.at
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# This script is under revision control on github.
+# https://github.com/gabkdlly/
+# Look under the repository on-the-clock .
+# I don't know enough about copyright to know be able to say what licence this
+# script should have, since not all the code is my own.
+# See the repository on the web for history and legal notices.
 
 from __future__ import division
 
@@ -63,7 +56,7 @@ def traverseData(items, month):
     # This is where all the fun starts !!
     runningTotal = 0
     tolerance = 0.2
-    breaks = 3600*1000
+    breaks = 60*60*1000
     startTime = 0
     stopTime = 0
     isCurrentlyAtWork = False
@@ -97,7 +90,7 @@ def traverseData(items, month):
             isCurrentlyAtWork = False
             stopTime = t
             runningTotal += (stopTime - startTime)
-            runningTotal -= breaks
+            runningTotal += breaks
     #f_out.close()
     print(str(month[0])+'-'+str(month[1]))
     print('    '+str( (-runningTotal)/(1000*3600) ))
